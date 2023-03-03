e42273193ba4d87e030e5940612354a0d00f38b6
sgram <126879375+yumiko-api@users.noreply.github.com>
2023-03-03 18:01:07 +0530

    Update __init__.py

--- sgram/__init__.py
+++ sgram/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.0.1"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
-__copyright__ = "Copyright (C) 2023-present Pr0fess0r-99 <https://github.com/Sgram-s>"
+__copyright__ = "Copyright (C) 2023-present yumiko-api <https://github.com/yumiko-api>"
 
