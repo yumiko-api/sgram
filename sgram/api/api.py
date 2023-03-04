c3e43ff0c746ff3e87f9d92b12ab6aca2c063361
sgram <126879375+yumiko-api@users.noreply.github.com>
2023-03-04 18:45:55 +0530

    Update api.py

--- sgram/api/api.py
+++ sgram/api/api.py
@@ -5,3 +5,8 @@ def chatbot(message):
     API = f"http://yumikoapi.herokuapp.com/chatbot?message={message}"
     req = requests.get(API).json()["url"]
     return(req)
+
+def wallpaper(message):
+    API = f"http://yumikoapi.herokuapp.com/wallpaper"
+    req = requests.get(API).json()["url"]
+    return(req)
